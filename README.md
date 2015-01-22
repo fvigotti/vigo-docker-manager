013cce6c9caf69ec3b360763c584c6e5865b0014
francesco <histats@gmail.com>
2015-01-22 15:49:45 +0000

    changing paradigm toward unique-app-container-name and shared image-name

--- README.md
+++ README.md
@@ -16,29 +16,29 @@ build an image from a local path, and call vmd_run to start the container after 
 the script also perform multiple (10) attempts if the docker-bug-mount-device-mapper occur
  
 
-**-name**  
- name of the docker image that is being built  
+**--img-name $name**    
+ name of the docker-image that is being built  
 
-**-version**  
+**--img-version $version**  
 value must be a valid number (will be checked by a regexp)  
 Version of the docker image that is being built (version will be used as docker image tag )  
 ie: ` -version "1.12"  
 
-**-path**  
- path of the dockerfile to build image from  
 
-**-runParam**  
-  additional params to provide to docker run command  
-  ie: ` -runParam "-d -p 8080:2812 " `  
+**--build-params  "$params"**  *[optional]*  
+  -> additional build params
 
-**-timeout**  
+**-timeout $seconds**  
  timeout value in seconds for docker stop (in case of docker stop command is used by restart action)  
 
-**--build-only** *[optional]*  
- do not start the image after build  
+**-path**  *[optional]*  
+ path of the dockerfile to build image from (if not specified will default to : /opt/docker_images/$imagename/$version/src )  
 
-**-do-restart** *[optional]*  
- restart the container after the deploy  
+**--build-only** *[optional]*  
+ do not try to start the image after the build   
+
+**--history-images-preserve-count** *[optional]*  
+ amount of docker-images to preserve as history, those images can be usefull to start already previous compiled version  
 
 
 ### ie:
@@ -54,25 +54,29 @@ stop previous version of the image-name running (if restart param is provided, e
  , perform multiple attempt to start the container and logs attempt results in `path/log/`  
  -d option for docker run is always implicit 
 
-
-**-path**  
- path of the dockerfile image, this is used to store logs and run-history-scripts
-    
-**-name**  
+**--img-name $name**  
  the docker image name that must be run , this will be combined with version to provided the full image:tag name   
-  
-**-do-restart** *[optional]*  
- restart the container if it's already running   
 
-**-version**  
- -> same as deploy  
+**--img-version $version** *[optional]*  
+ -> if not provided will default to latest built image version (using sort params : "sort --ignore-leading-blanks --version-sort -k2 -r" )  
  
-**-runParam**  
-  -> same as deploy
+**--run-params "$params"**  *[optional]*  
+  -> additional run params
 
-**-timeout**  
-  -> same as deploy
+**-timeout $seconds **  *[optional]*  
+  -> timeout used in case of restart is required 
 
+**--destroy-previous-container** *[optional]*  
+ destroy previous container if exists (contrarily it will simply restart it using "docker start" )    
+
+**--no-log** *[optional]*  
+ do not log the execution   
+
+**--no-detach** *[optional]*  
+ do not add "-d" to docker run    
+
+**--container-name $name**  *[optional]*  
+ name of the container, default=image name without special chars (which are not allowed) 
 
 # todo:
 refresh public registry hub cache before deploy  
--- src/vdm_deploy.sh
+++ src/vdm_deploy.sh
@@ -3,35 +3,42 @@ set -e
 
 echo -e '[ VIGO DOCKER MANAGER >>>> deploy ]'  >&2
 
-REMOVE_HEADER="tail -n +2"
+TAIL_DOCKERPS_HEADER="tail -n +2"
+
+BASE_VDM_IMAGES_PATH="/opt/docker_images/"
 
 print_usage ()
 {
-echo -e 'usage = \n'$0' [--do-restart , --do-rebuild]  -name APPNAME -version VERSION -runParam RUNPARAM -timeout TIMEOUT -path DOCKERFILEPATH\n\nExample:\n'$0' -name myapp -version 1.12 -runParam "-p 443:443 -p 80:80 -p 5922:22 -d" -timeout 10 -path="/opt/apptest/"'  >&2
+echo -e 'usage = \n'$0' [--do-rebuild]  -name APPNAME -version VERSION -path DOCKERFILEPATH\n\nExample:\n'$0' -name myapp -version 1.12 -runParam "-p 443:443 -p 80:80 -p 5922:22 -d" -timeout 10 -path="/opt/apptest/"'  >&2
 }
 
-if [ "5" -gt $# ] ; then
-    #echo 'usage = '$0' APPNAME VERSION PARAMS / ie: '$0' myapp v1.12 "-p 443:443 -p 80:80 -p 5922:22 -d" '
-    print_usage
-    exit -1
-fi
 
-#APP_NAME="t1_app1"
-#APP_VERSION="v1.3"
+#DOCKER_IMG_NAME="t1_app1"
+#DOCKER_IMG_VERSION="v1.3"
 #
-#APP_NAME=$1
-#APP_VERSION=$2
-#DOCKER_RUN_PARAMS=$3
+#DOCKER_IMG_NAME=$1
+#DOCKER_IMG_VERSION=$2
+#DOCKER_BUILD_PARAMS=$3
+# return image-sources-path ie: /opt/docker_images/testbashloop/1.1/src
+get_vdm_images_src_path (){
+local IMAGE_BASE_NAME=$1
+local IMAGE_TAG=$2
+echo ${BASE_VDM_IMAGES_PATH}''${IMAGE_BASE_NAME}'/'${IMAGE_TAG}'/src/'
+}
 
+clean_special_chars() {
+    local a=${1//[^[:alnum:]]/}
+    echo "${a,,}"
+}
 
 # DEFAULTS
 VDM_DEPLOY_SCRIPT_VERSION=1
-DEFAULT_BUILD_RETRY_ATTEMPT=10 #this is necessary because devicemapper occasionally fails to mount images during build process
-STOP_CONTAINER_TIMEOUT_SECONDS=3600
-DO_RESTART=0
-DO_REBUILD=0
+BUILD_RETRY_ATTEMPT=10 #this is necessary because devicemapper occasionally fails to mount images during build process
+STOP_CONTAINER_TIMEOUT_SECONDS=10
 CLEAN_GARBAGE=0
 DO_START=1
+IMAGES_HISTORY_PRESERVE_COUNT=3
+
 # vmd_deploy -name "dumbscript1" -version "v1.6" -runParam "-d -P" -timeout 10 -path "/opt/docker_dumbscr1/"
 # PARSE ARGS
 while [[ $# > 0 ]]
@@ -39,37 +46,32 @@ do
 key="$1"
 
 case $key in
-    -name|--name)
-    APP_NAME="$2"
+    --img-name)
+    DOCKER_IMG_NAME="$2"
+    shift
+    ;;
+    --img-version)
+    DOCKER_IMG_VERSION="$2"
     shift
     ;;
     -path|--path)
     DOCKER_IMAGE_PATH="$2"
     shift
     ;;
-    -version|--version)
-    APP_VERSION="$2"
+    --build-params) #optional
+    DOCKER_BUILD_PARAMS="$2"
     shift
     ;;
-    -runParam|--runParam)
-    DOCKER_RUN_PARAMS="$2"
-    shift
-    ;;
-    -timeout|--timeout)
+    -timeout|--timeout) #optional
     STOP_CONTAINER_TIMEOUT_SECONDS="$2"
     shift
     ;;
-    -do-restart|--do-restart)
-    DO_RESTART=1
+    --history-images-preserve-count) #optional
+    IMAGES_HISTORY_PRESERVE_COUNT="$2"
+    shift
     ;;
-    -do-rebuild|--do-rebuild)
-    DO_REBUILD=1
-    ;;
-    --clean-garbage)
+    --clean-garbage) #optional
     CLEAN_GARBAGE=1
-    ;;
-    --build-only)
-    DO_START=0
     ;;
     --default)
     DEFAULT=YES
@@ -81,31 +83,37 @@ esac
 shift
 done
 
-IMAGES_HISTORY_COUNT=3
-DOCKER_IMAGE_NAME=${APP_NAME}":"${APP_VERSION}
+DOCKER_IMAGE_NAME_AND_VERSION=${DOCKER_IMG_NAME}":"${DOCKER_IMG_VERSION}
 
 CURPATH=$(pwd)
 CUR_EPOCH=$(date +%s)
 
 echo 'CURPATH = '${CURPATH}\
 ' , DOCKER_IMAGE_PATH='${DOCKER_IMAGE_PATH}\
-' , APP_NAME='${APP_NAME}\
-', APP_VERSION = '${APP_VERSION}\
-' , DOCKER_RUN_PARAMS = '${DOCKER_RUN_PARAMS}\
+' , DOCKER_IMG_NAME='${DOCKER_IMG_NAME}\
+', DOCKER_IMG_VERSION = '${DOCKER_IMG_VERSION}\
+' , DOCKER_BUILD_PARAMS = '${DOCKER_BUILD_PARAMS}\
 ' , STOP_CONTAINER_TIMEOUT_SECONDS = '${STOP_CONTAINER_TIMEOUT_SECONDS}\
 ' , CUR_EPOCH = '${CUR_EPOCH}  >&2
 
-if [ -z "$APP_NAME" ] || ! [[ "$APP_NAME" =~ ^(.+)$ ]]; then
- echo -e 'invalid APP_NAME :'$APP_NAME')' >&2
+if [ -z "$DOCKER_IMG_NAME" ] || ! [[ "$DOCKER_IMG_NAME" =~ ^(.+)$ ]]; then
+ echo -e 'invalid DOCKER_IMG_NAME :'$DOCKER_IMG_NAME')' >&2
      print_usage
     exit -1
 fi
 
-if [ -z "$APP_VERSION" ] || ! [[ "$APP_VERSION" =~ ^([0-9\.]+)$ ]]; then
- echo -e 'invalid APP_VERSION :'$APP_VERSION')' >&2
+if [ -z "$DOCKER_IMG_VERSION" ] || ! [[ "$DOCKER_IMG_VERSION" =~ ^([0-9\.]+)$ ]]; then
+ echo -e 'invalid DOCKER_IMG_VERSION :'$DOCKER_IMG_VERSION')' >&2
     print_usage
     exit -1
 fi
+
+
+
+[ -z "$DOCKER_IMAGE_PATH" ] && {
+    DOCKER_IMAGE_PATH=$( get_vdm_images_src_path $DOCKER_IMG_NAME $DOCKER_IMG_VERSION )
+    echo 'docker-image-soruces path defaulted to :'${DOCKER_IMAGE_PATH} >&2
+}
 
 if [ -z "$DOCKER_IMAGE_PATH" ] || ! [[ "$DOCKER_IMAGE_PATH" =~ ^/(.+)/$ ]]; then
  echo -e 'invalid DOCKER_IMAGE_PATH :'$DOCKER_IMAGE_PATH' , required string between slashes' >&2
@@ -149,7 +157,7 @@ search_image_and_tag () {
 }
 
 get_sorted_images () {
- docker images  | awk '$1 ~ "'${APP_NAME}'" {print $0}' | sort --ignore-leading-blanks --version-sort -k2 -r
+ docker images  | awk '$1 ~ "'${DOCKER_IMG_NAME}'" {print $0}' | sort --ignore-leading-blanks --version-sort -k2 -r
 }
 
  get_stopped_containers_ids_for_image_id () {
@@ -158,7 +166,7 @@ if [ -z "$1" ] || ! [[ "$1" =~ ^[0-9a-f]+$ ]]; then
  exit -1
 fi
 docker ps -a --filter "status=exited" \
-| $REMOVE_HEADER \
+| $TAIL_DOCKERPS_HEADER \
 | awk '$2 ~ "'$1'" {print $1}'
 
 }
@@ -169,7 +177,7 @@ if [ -z "$1" ] || ! [[ "$1" =~ ^(.+):(.+)$ ]]; then
  exit -1
 fi
 docker ps -a --filter "status=exited" \
-| $REMOVE_HEADER \
+| $TAIL_DOCKERPS_HEADER \
 | awk '$2 ~ "'$1'" {print $0}'
 
 }
@@ -183,37 +191,35 @@ sync
 }
 
 get_deprecated_oldest_images_name_and_tag () {
-if [ -z "$1" ]; then
- echo -e 'param missing  amount of images to preserve on history is required ' >&2
- exit -1
-fi
-local ROLLBACK_IMAGES_TO_KEEP=$(($1)) #remember that tail -n strip the first row, and :latest image is also included (when ROLLBACK_IMAGES_TO_KEEP=7 , 5 real rollback images are kept )
-get_sorted_images | $REMOVE_HEADER | tail -n +"${ROLLBACK_IMAGES_TO_KEEP}" | awk '{print $1":"$2}'
+    if [ -z "$1" ]; then
+        echo -e 'param missing  amount of images to preserve on history is required ' >&2
+        exit -1
+    fi
+    local ROLLBACK_IMAGES_TO_KEEP=$(($1)) #remember that tail -n strip the first row, and :latest image is also included (when ROLLBACK_IMAGES_TO_KEEP=7 , 5 real rollback images are kept )
+
+    get_sorted_images | $TAIL_DOCKERPS_HEADER | tail -n +"${ROLLBACK_IMAGES_TO_KEEP}" | awk '{print $1":"$2}'
 }
 
 remove_oldest_images () {
+    local PRESERVE_COUNT=$1
     sync
-    echo -e '>>> deleting stopped container for deprecated images  preserving '$1' images in history'  >&2
+    echo -e '>>> deleting stopped container for deprecated images  preserving '${PRESERVE_COUNT}' images in history'  >&2
     #docker ps -a | awk '$2 ~ "a:3" {print $0}'
-    get_deprecated_oldest_images_name_and_tag $1 \
+    get_deprecated_oldest_images_name_and_tag ${PRESERVE_COUNT} \
     | while read -r imageIdToRemove; do
 
-        local IMAGE_TO_REMOVE_CURRENTLY_RUNNING=$(get_running_containers_for_image ${imageIdToRemove} "0" |wc -l)
-        echo '$IMAGE_TO_REMOVE_CURRENTLY_RUNNING = '$IMAGE_TO_REMOVE_CURRENTLY_RUNNING  >&2
+        local IMAGE_TO_REMOVE_IS_CURRENTLY_RUNNING_count=$(get_running_containers_for_image ${imageIdToRemove} "0" |wc -l)
+        echo '$IMAGE_TO_REMOVE_IS_CURRENTLY_RUNNING_count = '$IMAGE_TO_REMOVE_IS_CURRENTLY_RUNNING_count  >&2
 
-        # if image container is not currently running
-        if [ "$IMAGE_TO_REMOVE_CURRENTLY_RUNNING" -gt "0" ]; then
-            echo '$IMAGE_TO_REMOVE_CURRENTLY_RUNNING (list version)= '$(get_running_containers_for_image ${imageIdToRemove} "0" )  >&2
-            # if forcing restart OR
-            if [[ $DO_RESTART = 1 ]]; then
-               echo '[RESTART] force stop of running image because deprecated '${imageIdToRemove}  >&2
-               get_running_containers_for_image ${imageIdToRemove} "1"  | xargs --no-run-if-empty docker stop -t $STOP_CONTAINER_TIMEOUT_SECONDS
-               sync
-            else
-               echo '[WARNING] skip deletion of deprecated image: '${imageIdToRemove} ' because running'  >&2
-               continue #skip this image
-               echo '[FATAL ERROR ] POST CONTINNUE MESSAGE'  >&2
-            fi
+        # if image container currently running
+        if [ "$IMAGE_TO_REMOVE_IS_CURRENTLY_RUNNING_count" -gt "0" ]; then
+
+            echo '$IMAGE_TO_REMOVE_IS_CURRENTLY_RUNNING_count (list version)= '$(get_running_containers_for_image ${imageIdToRemove} "0" )  >&2
+
+            # if restart has been requested, stop the running container
+            echo '[WARNING] skip deletion of deprecated image: '${imageIdToRemove} ' because running'  >&2
+            continue #skip this image
+            echo '[FATAL ERROR ] POST CONTINNUE MESSAGE'  >&2
         fi
 
         echo -e 'remove_stopped_containers_for_image '$imageIdToRemove ;  >&2
@@ -224,20 +230,8 @@ remove_oldest_images () {
 
     sync
 }
-#
-#remove_dangling_images ()
-#{
-##CONTAINERS_TO_REMOVE_COUNT=$1 # argv 1
-##docker ps -a  | grep ${APP_NAME} | awk '{print $1}' | tail -"${CONTAINERS_TO_REMOVE_COUNT}" | while read -r id; do docker rm $id ; done
-## ( tail will strip out the header of the output)
-#docker images --filter "dangling=true" | awk '$3 ~ "[0-9a-f]{12}" {print $3}' | while read -r id; do
-#echo -e 'deleting image "'$id'" stopped containers ';
-#get_stopped_containers_ids_for_image_id $id | xargs --no-run-if-empty docker rm
-#echo -e 'deleting image: '$id;
-#docker rmi $id ;
-#done
-#}
 
+# deletes docker-images that have "<none>" as assigned name (usually interrupted build processes or cached intermediate containers )
 remove_nonamed_images () {
 
 sync
@@ -256,9 +250,9 @@ sync
 
 build_image () {
 local MAX_RETRY_COUNT=$1
-# same thing? :)
-#remove_dangling_images
+echo 'building image, MAX_RETRY_COUNT='$MAX_RETRY_COUNT >&2
 sync
+
 #TEMP DISABLED TO AVOID CHACHE PURGE
 [[ $CLEAN_GARBAGE = 1 ]] && remove_nonamed_images
 
@@ -272,11 +266,11 @@ sync
 ## build image with cleanup of intermediate containers (  docker build -rm=true )
 
 echo "Purge deprecated version images:" >&2
-remove_oldest_images $IMAGES_HISTORY_COUNT
+remove_oldest_images $IMAGES_HISTORY_PRESERVE_COUNT
 sync
 
 
-local BUILD_LOG_FILENAME='vdm_build_'${APP_NAME}':'${APP_VERSION}'.log'
+local BUILD_LOG_FILENAME='vdm_build_'${DOCKER_IMG_NAME}':'${DOCKER_IMG_VERSION}'.log'
 
 # build output log directory if not exists
 [ -d "${OUTPUT_LOG_PATH}" ] || mkdir "${OUTPUT_LOG_PATH}"
@@ -290,25 +284,23 @@ echo -e '[out] $n='$n' ,$MAX_RETRY_COUNT='$MAX_RETRY_COUNT
    echo -e '$n='$n' ,$MAX_RETRY_COUNT='$MAX_RETRY_COUNT >&2
    echo -e '[out] $n='$n' ,$MAX_RETRY_COUNT='$MAX_RETRY_COUNT
       local buildResults=0;
-      echo '>>> Building new image docker build -t="'${APP_NAME}':'${APP_VERSION}'" . >'$BUILD_LOG_FILENAME_AND_PATH' 2>&1'  >&2
+      echo '>>> Building new image docker build  '${DOCKER_BUILD_PARAMS}' -t="'${DOCKER_IMG_NAME}':'${DOCKER_IMG_VERSION}'" . >'$BUILD_LOG_FILENAME_AND_PATH' 2>&1'  >&2
 
       # Build image and capture possible errors
       # --no-cache
-      docker build -t="${APP_NAME}:${APP_VERSION}" . >$BUILD_LOG_FILENAME_AND_PATH 2>&1 || buildResults=$?
+      docker build ${DOCKER_BUILD_PARAMS} -t="${DOCKER_IMG_NAME}:${DOCKER_IMG_VERSION}" . >$BUILD_LOG_FILENAME_AND_PATH 2>&1 || buildResults=$?
 
-        if [[ "$buildResults" != "0" ]]; # BUILD FAILED
-        then
-           if ! grep -q "from driver devicemapper: open" $BUILD_LOG_FILENAME_AND_PATH  && ! grep -q "from driver devicemapper: Error mounting" $BUILD_LOG_FILENAME_AND_PATH ; then # KNOW COMMON ERROR
+        if [[ "$buildResults" != "0" ]]; then # BUILD FAILED
+            # if it's not the bug of devicemapper-mount-fail
+           if ! grep -q "from driver devicemapper: open" $BUILD_LOG_FILENAME_AND_PATH  && ! grep -q "from driver devicemapper: Error mounting" $BUILD_LOG_FILENAME_AND_PATH ; then
                 echo '[FATAL ERROR] >>> build attempt '${n}' failed, but the error was not a mounting issue'  >&2
                 cat $BUILD_LOG_FILENAME_AND_PATH
-
                 exit 1
             else
               n=$[$n+1]
               echo '>>> build attempt '${n}' failed, retrying'  >&2
               sleep 2
            fi
-
         else # BUILD SUCCESS
          echo '[SUCCESS]  image built successfully !'  >&2
          cat $BUILD_LOG_FILENAME_AND_PATH
@@ -316,29 +308,21 @@ echo -e '[out] $n='$n' ,$MAX_RETRY_COUNT='$MAX_RETRY_COUNT
         fi
    done
 
-}
+} # build_image
+
+
+
 ############### BODY
 
 echo '[START] VDM_DEPLOY_SCRIPT_VERSION='$VDM_DEPLOY_SCRIPT_VERSION  >&2
 echo '[CHDIR] '$DOCKER_IMAGE_PATH  >&2
 cd $DOCKER_IMAGE_PATH
 
-ALREADY_BUILT_COUNTER=$(search_image_and_tag ${APP_NAME} ${APP_VERSION} |wc -l)
+ALREADY_BUILT_COUNTER=$( search_image_and_tag ${DOCKER_IMG_NAME} ${DOCKER_IMG_VERSION} | wc -l )
 echo '$ALREADY_BUILT_COUNTER = '$ALREADY_BUILT_COUNTER  >&2
 if [ "$ALREADY_BUILT_COUNTER" -gt "0" ]; then
-   echo '[SKIP] image already exists for '${APP_NAME}' '${APP_VERSION}', skipping build'  >&2
+   echo '[SKIP] image already exists for '${DOCKER_IMG_NAME}' '${DOCKER_IMG_VERSION}', skipping build'  >&2
 else
-    build_image $DEFAULT_BUILD_RETRY_ATTEMPT
+    build_image $BUILD_RETRY_ATTEMPT
 fi
 
-
-
-
-
-# build vdm_run command
-OPTS_VDM_RUN=""
-[[ $DO_RESTART = 1 ]] && OPTS_VDM_RUN=$OPTS_VDM_RUN" -do-restart" || a="$d"
-[[ $DO_START -eq "1" ]] && {
-    echo '[EXECUTING] vdm_run '${OPTS_VDM_RUN}' -name "'${APP_NAME}'" -version "'${APP_VERSION}'" -runParam "'${DOCKER_RUN_PARAMS}'" -timeout "'${STOP_CONTAINER_TIMEOUT_SECONDS}'" -path "'${DOCKER_IMAGE_PATH}'"'  >&2
-    vdm_run $OPTS_VDM_RUN -name "${APP_NAME}" -version "${APP_VERSION}" -runParam "${DOCKER_RUN_PARAMS}" -timeout "${STOP_CONTAINER_TIMEOUT_SECONDS}" -path "${DOCKER_IMAGE_PATH}"
-}
