cba8a8fc49389f2545116012894e54ee7f93c510
francesco <histats@gmail.com>
2015-01-23 14:29:40 +0000

    pre-tested script

--- src/vdm_run.sh
+++ src/vdm_run.sh
@@ -17,7 +17,10 @@ DOCKER_DETACHED_MODE="-d"
 OPTIONS_DESTROY_PREVIOUS_CONTAINER=0
 TMP_DIR="/tmp/"
 DOCKER_RUN_LOG_COMMENT=""
-
+DOCKER_RUN_ADDITIONAL_PARAMS=''
+DOCKER_IMAGE_VERSION=''
+DOCKER_IMAGE_UNIQUE_ID_OR_NAMEANDVERSION=''
+MAX_CONTAINER_RUN_ATTEMPT_COUNT=5
 
 clean_special_chars() {
     local a=${1//[^[:alnum:]]/}
@@ -45,7 +48,7 @@ case $key in
     shift
     ;;
     --run-params) #optional
-    DOCKER_RUN_PARAMS="$2"
+    DOCKER_RUN_ADDITIONAL_PARAMS="$2"
     shift
     ;;
     -timeout|--timeout) #optional
@@ -72,39 +75,12 @@ shift
 done
 
 CONTAINER_HISTORY_PRESERVE_COUNT=4
-DOCKER_IMAGE_NAME=${DOCKER_IMAGE_NAME}":"${DOCKER_IMAGE_VERSION}
+
 
 CUR_EPOCH=$(date +%s)
 
-
-
-
-
-
-
-
-echo 'DOCKER_IMAGE_NAME='${DOCKER_IMAGE_NAME}\
-', DOCKER_IMAGE_VERSION = '${DOCKER_IMAGE_VERSION}\
-' , DOCKER_RUN_PARAMS = '${DOCKER_RUN_PARAMS}\
-' , STOP_CONTAINER_TIMEOUT_SECONDS = '${STOP_CONTAINER_TIMEOUT_SECONDS}\
-' , DOCKER_CONTAINER_INSTANCE_NAME = '${DOCKER_CONTAINER_INSTANCE_NAME}\
-' , CUR_EPOCH = '${CUR_EPOCH} >&2
-# ps -a  | grep ${DOCKER_IMAGE_NAME} | awk '{print $1}' | tail -2 | while read -r id; do docker rm $id ; done
-
-if [ -z "$DOCKER_IMAGE_NAME" ] || ! [[ "$DOCKER_IMAGE_NAME" =~ ^(.+)$ ]]; then
- echo -e 'invalid DOCKER_IMAGE_NAME :'$DOCKER_IMAGE_NAME')' >&2
-     print_usage
-    exit -1
-fi
-
-
-OUTPUT_LOG_PATH=$DOCKER_IMAGE_PATH'/log/'
-
 ##################  HEADER
 
-get_default_instance_name () {
-s=0
-}
 
 containers_created () {
 docker ps -a  | awk '$2 ~ "^'${DOCKER_IMAGE_NAME}':(.*)$" {print $0}' | $REMOVE_HEADER
@@ -174,8 +150,50 @@ docker_restart_container () {
     return $ret
 }
 
-docker_start_container() {
-   echo "todo"
+# return docker command to run the container
+#
+get_docker_start_container_CMD() {
+   echo 'docker run '$DOCKER_DETACHED_MODE' '$DOCKER_RUN_ADDITIONAL_PARAMS' --name="'${CONTAINER_NAME}'" '${DOCKER_IMAGE_UNIQUE_ID_OR_NAMEANDVERSION}
+}
+
+run_docker_cmd_multiple_attempt(){
+    local retry_counter=0
+    local docker_run_success=0
+
+    DOCKER_CMD=$( get_docker_start_container_CMD )
+    echo 'DOCKER_CMD = '$DOCKER_CMD  >&2
+
+
+
+    MAX_CONTAINER_RUN_ATTEMPT_COUNT=5
+    set +e
+    set -o pipefail
+    until [ "$retry_counter" -ge "$MAX_CONTAINER_RUN_ATTEMPT_COUNT" ]
+       do
+           sync
+           echo -e '[EXECUTING] '$DOCKER_CMD >&2
+           $DOCKER_RUN_CMD 2>&1 | tee -a $DOCKER_RUN_LOG
+           docker_run_success=$?
+           sync
+
+           if [[ "$docker_run_success" != "0" ]]; # RUN FAILED
+           then
+               retry_counter=$[$retry_counter+1]
+               echo '>>> run attempt '${retry_counter}' failed, retrying'  >&2
+               sleep 2
+           else # BUILD SUCCESS
+               echo '[SUCCESS]  program started successfully (attempt number = '${retry_counter}')!'  >&2
+               tail -10 $DOCKER_RUN_LOG
+               set -e
+               exit 0
+               break;
+           fi
+       done
+    echo '[FATAL-ERROR] run failed after '$retry_counter' attempt' >&2
+    set -e
+    tail -40 $DOCKER_RUN_LOG
+    set -e
+    return 1
 }
 
 count_containers_created () {
@@ -197,36 +215,42 @@ search_app_instances_running () {
     local IMAGE_NAME=$1
     docker ps | awk '$2 ~ "^\\s*'${IMAGE_NAME}':.*$" {print $0}'
 }
-build_vdm_run_version_script() {
-local VDM_RESTARTER_SCRIPT_FILENAME='vdm_restart_'$DOCKER_IMAGE_NAME'.sh'
-local VDM_RESTARTER_SCRIPT_FILENAME=${VDM_RESTARTER_SCRIPT_FILENAME//":"/"_"} #replace -> : <- with -> _ <- in filename
-
-# build output log directory if not exists
-[ -d "${OUTPUT_LOG_PATH}" ] || mkdir "${OUTPUT_LOG_PATH}"
-local VDM_RESTARTER_SCRIPT_FILENAME_AND_PATH=$OUTPUT_LOG_PATH''$VDM_RESTARTER_SCRIPT_FILENAME
-
-
-if [ ! -e "$VDM_RESTARTER_SCRIPT_FILENAME_AND_PATH" ]; then
-        echo -e 'creating restarter script : '$VDM_RESTARTER_SCRIPT_FILENAME_AND_PATH >&2
-VDM_RESTARTER_SCRIPT_CONTENT='
-#!/usr/bin/env bash\n
-set -e\n
-vdm_run -name "'$DOCKER_IMAGE_NAME'" -version "'$DOCKER_IMAGE_VERSION'" -runParam "'$DOCKER_RUN_PARAMS'" -timeout "'$STOP_CONTAINER_TIMEOUT_SECONDS'" -path "'$DOCKER_IMAGE_PATH'" $@ \n
-'
-        echo -e $VDM_RESTARTER_SCRIPT_CONTENT > $VDM_RESTARTER_SCRIPT_FILENAME_AND_PATH
-        chmod +x $VDM_RESTARTER_SCRIPT_FILENAME_AND_PATH
-    fi
-}
 
 ##################  BODY
 
-
 echo '[START] VDM_RUN_SCRIPT_VERSION='$VDM_RUN_SCRIPT_VERSION >&2
-echo '[CHDIR] '$DOCKER_IMAGE_PATH >&2
-cd $DOCKER_IMAGE_PATH
+
+echo 'DOCKER_IMAGE_NAME='${DOCKER_IMAGE_NAME}\
+', DOCKER_IMAGE_VERSION = '${DOCKER_IMAGE_VERSION}\
+' , DOCKER_RUN_PARAMS = '${DOCKER_RUN_ADDITIONAL_PARAMS}\
+' , STOP_CONTAINER_TIMEOUT_SECONDS = '${STOP_CONTAINER_TIMEOUT_SECONDS}\
+' , DOCKER_CONTAINER_INSTANCE_NAME = '${DOCKER_CONTAINER_INSTANCE_NAME}\
+' , CUR_EPOCH = '${CUR_EPOCH} >&2
+# ps -a  | grep ${DOCKER_IMAGE_NAME} | awk '{print $1}' | tail -2 | while read -r id; do docker rm $id ; done
+
+if [ -z "$DOCKER_IMAGE_NAME" ] || ! [[ "$DOCKER_IMAGE_NAME" =~ ^(.+)$ ]]; then
+    echo -e 'invalid DOCKER_IMAGE_NAME :'$DOCKER_IMAGE_NAME')' >&2
+    print_usage
+    exit -1
+fi
+
+# check existance of provided docker image
+[[ -z "$(get_find_docker_images $DOCKER_IMAGE_NAME 3)" ]] && {
+    echo '[ERROR] '$DOCKER_IMAGE_NAME' not found, exiting '  >&2
+    exit 3
+}
 
 
+if [ -z "$DOCKER_IMAGE_VERSION" ]; then
+    echo 'DOCKER_IMAGE_VERSION not specified, searching for higher version number... ' >&2
+    DOCKER_IMAGE_VERSION=$( $(get_sorted_images $image_name) | head -1 |awk '{print $2}' )
+    echo 'DOCKER_IMAGE_VERSION found = '$DOCKER_IMAGE_VERSION >&2
+fi
+DOCKER_IMAGE_UNIQUE_ID_OR_NAMEANDVERSION=$DOCKER_IMAGE_NAME':'$DOCKER_IMAGE_VERSION
+echo 'DOCKER_IMAGE_UNIQUE_ID_OR_NAMEANDVERSION = '$DOCKER_IMAGE_UNIQUE_ID_OR_NAMEANDVERSION >&2
 
+DOCKER_RUN_LOG='/var/log/docker/run_'${DOCKER_IMAGE_NAME}'.log'
+echo 'DOCKER_RUN_LOG = '$DOCKER_RUN_LOG >&2
 
 echo 'checking if container '$CONTAINER_NAME' is already running ' >&2
 if ! [[ -z "$(get_running_container_id_from_name $CONTAINER_NAME )" ]]; then
@@ -258,123 +282,19 @@ if ! [[ -z "$(get_stopped_or_started_container_id_from_name $CONTAINER_NAME )" ]
 fi
 
 
+
 echo 'checking if is not in started or stopped state before starting it ' >&2
 if [[ -z "$(get_stopped_or_started_container_id_from_name $CONTAINER_NAME )" ]]; then
     echo 'container '$CONTAINER_NAME' will now be started ' >&2
+
+    run_docker_cmd_multiple_attempt
+
 fi
 
 
-DOCKER_IMAGE_TO_RUN
-# check existance of provided docker image
-[[ -z "$(get_find_docker_images $DOCKER_IMAGE_NAME 3)" ]] && {
-echo '[ERROR] '$DOCKER_IMAGE_NAME' not found, exiting '  >&2
-exit 3
-}
-
-
-DOCKER_IMAGE_VERSION
-
-
-
-
-build_vdm_run_version_script
-
-ALREADY_RUNNING_CONTAINER=$(search_app_instances_running ${DOCKER_IMAGE_NAME})
-ALREADY_RUNNING_COUNTER=$(search_app_instances_running ${DOCKER_IMAGE_NAME} |wc -l)
-echo '$ALREADY_RUNNING_COUNTER = '$ALREADY_RUNNING_COUNTER' , $DO_RESTART='$DO_RESTART >&2
-if [ "$ALREADY_RUNNING_COUNTER" -gt "0" ]; then
-   if [ "$DO_RESTART" -gt "0" ]; then
-    echo '[RESTART] app container already running =  '${DOCKER_IMAGE_NAME}' '${DOCKER_IMAGE_VERSION}', stopping instances ' >&2
-    search_app_instances_running ${DOCKER_IMAGE_NAME} | awk   '{print $1}' | xargs --no-run-if-empty docker stop -t $STOP_CONTAINER_TIMEOUT_SECONDS
-   else
-    echo '[ALREADY RUNNING] app container already running =  '${ALREADY_RUNNING_CONTAINER} >&2
-    exit 0
-   fi
-else
-    echo '>>> no previous instances running' >&2
-fi
-
-echo ">>> purge old containers" >&2
-CONTAINER_CREATED_COUNT=$(count_containers_created)
-CONTAINERS_TO_REMOVE_COUNT=$((CONTAINER_CREATED_COUNT-CONTAINER_HISTORY_PRESERVE_COUNT))
-echo 'containers created count : '${CONTAINER_CREATED_COUNT}' , to remove = '${CONTAINERS_TO_REMOVE_COUNT} >&2
-if [ "$CONTAINERS_TO_REMOVE_COUNT" -gt "0" ]
-then
- remove_oldest_n_containers $CONTAINERS_TO_REMOVE_COUNT
-fi
-
-
-
-#
-#echo '>>> Stop previous version of this app'
-#if [ "$DRY_RUN" -eq "0" ]; then
-#sync
-#docker ps --filter "name="${DOCKER_IMAGE_NAME} -q | xargs --no-run-if-empty docker stop -t $STOP_CONTAINER_TIMEOUT_SECONDS
-#sleep 1
-#sync
-#sleep 1
-#else
-#echo -e '[DRY-RUN]\n docker ps --filter "name="'${DOCKER_IMAGE_NAME}' -q | xargs --no-run-if-empty docker stop -t '$STOP_CONTAINER_TIMEOUT_SECONDS
-#fi
-
-echo '>>> Starting container docker run -d '$DOCKER_RUN_PARAMS' --name="'${DOCKER_CONTAINER_INSTANCE_NAME}'" '${DOCKER_IMAGE_NAME} >&2
-
-if [ "$DRY_RUN" -eq "0" ]; then
-docker ps --filter "name="${DOCKER_IMAGE_NAME} -q | xargs --no-run-if-empty docker stop -t $STOP_CONTAINER_TIMEOUT_SECONDS
-sync
-else
-echo -e '[DRY-RUN]\n docker ps --filter "name="'${DOCKER_IMAGE_NAME}' -q | xargs --no-run-if-empty docker stop -t '$STOP_CONTAINER_TIMEOUT_SECONDS >&2
-fi
-
-
-
-if [ "$DRY_RUN" -eq "0" ]; then
-    [ -d "${OUTPUT_LOG_PATH}" ] || mkdir "${OUTPUT_LOG_PATH}"
-    RUN_LOG=$OUTPUT_LOG_PATH'vdm_run_'${DOCKER_IMAGE_NAME}'_'${DOCKER_IMAGE_VERSION}'.log'
-    # reset run log
-    echo "" > $RUN_LOG
-
-    n=0
-
-
-    MAX_RETRY_COUNT=5
-    set +e
-    set -o pipefail
-    until [ "$n" -ge "$MAX_RETRY_COUNT" ]
-       do
-
-           sync
-           rebuild_instance_name
-           echo -e '[EXECUTING]\ndocker run  -d '$DOCKER_RUN_PARAMS' --name="'${DOCKER_CONTAINER_INSTANCE_NAME}'" '${DOCKER_IMAGE_NAME} >&2
-           docker run -d $DOCKER_RUN_PARAMS --name="${DOCKER_CONTAINER_INSTANCE_NAME}" ${DOCKER_IMAGE_NAME} 2>&1 | tee -a $RUN_LOG
-           buildResults=$?
-
-           sync
-
-           if [[ "$buildResults" != "0" ]]; # RUN FAILED
-           then
-               n=$[$n+1]
-               echo '>>> run attempt '${n}' failed, retrying'  >&2
-               sleep 2
-           else # BUILD SUCCESS
-               echo '[SUCCESS]  program started successfully (attempt '${n}')!'  >&2
-               tail -10 $RUN_LOG
-               set -e
-               exit 0
-               break;
-           fi
-       done
-    echo '[FATAL-ERROR] run failed after '$n' attempt' >&2
-    set -e
-    tail -40 $RUN_LOG
-    set -e
-    return 1
-
-else
-echo -e '[DRY-RUN]\ndocker run  -d '$DOCKER_RUN_PARAMS' --name="'${DOCKER_CONTAINER_INSTANCE_NAME}'" '${DOCKER_IMAGE_NAME} >&2
-fi
-
-
+### closing parts
+echo 'unsuccessfull end of the script, this row should never get printed'  >&2
+echo 'unsuccessfull end of the script, this row should never get printed'
 
 
 
